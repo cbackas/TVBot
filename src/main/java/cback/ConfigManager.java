8443dd7e28f9b0c155f9d0505f4c8557ebd87672
cbackas <cbackasSE@gmail.com>
2016-10-11 12:37:46 -0500

    Adds movienight stuff to the default config

--- src/main/java/cback/ConfigManager.java
+++ src/main/java/cback/ConfigManager.java
@@ -23,6 +23,9 @@ public class ConfigManager {
         //Insert all default config values here. They will be added on startup if they do not exist.
         defaultConfig.put("botToken", "TOKEN");
         defaultConfig.put("traktToken", "TOKEN");
+        defaultConfig.put("pollID", "POLLID");
+        defaultConfig.put("date", "movienight date/time");
+        defaultConfig.put("mnID", "movienight announce messageID");
     }
 
     public ConfigManager(TVBot bot) {
