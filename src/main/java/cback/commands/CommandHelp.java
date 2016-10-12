25824b5be98955bfbe7204ec28b452fed38cdbd0
cbackas <cbackasSE@gmail.com>
2016-10-12 10:23:39 -0500

    Updates help command, makes getting RoleIDs easier

--- src/main/java/cback/commands/CommandHelp.java
+++ src/main/java/cback/commands/CommandHelp.java
@@ -6,10 +6,12 @@ import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.internal.DiscordUtils;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
+import sx.blah.discord.handle.obj.IRole;
 import sx.blah.discord.handle.obj.Permissions;
 import sx.blah.discord.util.MessageBuilder;
 
 import java.util.EnumSet;
+import java.util.List;
 
 public class CommandHelp implements Command {
     @Override
@@ -20,27 +22,46 @@ public class CommandHelp implements Command {
     @Override
     public void execute(TVBot bot, IDiscordClient client, String[] args, IGuild guild, IMessage message, boolean isPrivate) {
         String userCommands =
-                "!help                              //shows a list of commands\n" +
-                "!goodnight                         //does that good night thing everyone likes\n" +
-                "!lenny                             //lenny\n" +
-                "!shrug                             //shrugs";
+                "!help                                  //shows a list of commands\n" +
+                "!goodnight                             //does that good night thing everyone likes\n" +
+                "!lenny                                 //lenny\n" +
+                "!shrug                                 //shrugs";
 
         String modCommands =
-                "!addshow [imdbid] [here|channelid] //adds a new show to the calendar\n" +
-                "!removeshow [imdbid]               //deletes a show from the calendar\n" +
-                "!addlog [message]                  //adds a message to the log\n" +
-                "!mute @user                        //mutes user\n" +
-                "!unmute @user                      //unmutes user\n" +
-                "!kick @user [reason]               //kicks user and logs the action\n" +
-                "!ban @user [reason]                //bans user and logs the action\n" +
-                "!rule [number]                     //posts the rule requested in chat";
-        try {
-            DiscordUtils.checkPermissions(message.getChannel().getModifiedPermissions(message.getAuthor()), EnumSet.of(Permissions.BAN));
-            new MessageBuilder(client).withChannel(message.getAuthor().getOrCreatePMChannel()).appendQuote("TVBot's Commands:").appendCode("XL", userCommands).appendCode("XL", modCommands).appendQuote("Mod+ commands included - regular users can not see staff commands").send();
-        } catch (Exception e) {
+                "!addshow [imdbid] [here|channelid]     //adds a new show to the calendar\n" +
+                "!removeshow [imdbid]                   //deletes a show from the calendar\n" +
+                "!addlog [message]                      //adds a message to the log\n" +
+                "!mute @user                            //mutes user\n" +
+                "!unmute @user                          //unmutes user\n" +
+                "!kick @user [reason]                   //kicks user and logs the action\n" +
+                "!ban @user [reason]                    //bans user and logs the action\n" +
+                "!rule [number]                         //posts the rule requested in chat";
+
+        String movieCommands =
+                "!movienight set [pollID] [date]        //posts a link to a the google poll\n" +
+                        "!movienight announce [movie]           //deletes poll and announces movie\n" +
+                        "!movienight start [rabbitID]           //announces movienight start and links to room";
+
+        List<IRole> roles = message.getAuthor().getRolesForGuild(guild);
+        if (roles.contains(guild.getRoleByID("226443478664609792")) || roles.contains(guild.getRoleByID("192441946210435072"))) {
             try {
-                new MessageBuilder(client).withChannel(message.getAuthor().getOrCreatePMChannel()).appendQuote("TVBot's Commands:").appendCode("XL", userCommands).appendQuote("This bot will get some cool commands down the road").send();
-            } catch (Exception f) {
+                DiscordUtils.checkPermissions(message.getChannel().getModifiedPermissions(message.getAuthor()), EnumSet.of(Permissions.BAN));
+                new MessageBuilder(client).withChannel(message.getAuthor().getOrCreatePMChannel()).appendQuote("TVBot's Commands:").appendCode("XL", userCommands).appendCode("XL", modCommands).appendCode("XL", movieCommands).appendQuote("Mod+ commands included - regular users can not see staff commands").send();
+            } catch (Exception e) {
+                try {
+                    new MessageBuilder(client).withChannel(message.getAuthor().getOrCreatePMChannel()).appendQuote("TVBot's Commands:").appendCode("XL", userCommands).appendCode("XL", movieCommands).appendQuote("This bot will get some cool commands down the road").send();
+                } catch (Exception f) {
+                }
+            }
+        } else {
+            try {
+                DiscordUtils.checkPermissions(message.getChannel().getModifiedPermissions(message.getAuthor()), EnumSet.of(Permissions.BAN));
+                new MessageBuilder(client).withChannel(message.getAuthor().getOrCreatePMChannel()).appendQuote("TVBot's Commands:").appendCode("XL", userCommands).appendCode("XL", modCommands).appendQuote("Mod+ commands included - regular users can not see staff commands").send();
+            } catch (Exception e) {
+                try {
+                    new MessageBuilder(client).withChannel(message.getAuthor().getOrCreatePMChannel()).appendQuote("TVBot's Commands:").appendCode("XL", userCommands).appendQuote("This bot will get some cool commands down the road").send();
+                } catch (Exception f) {
+                }
             }
         }
         Util.deleteMessage(message);
---
+++ src/main/java/cback/commands/CommandRoleID.java
@@ -0,0 +1,44 @@
+package cback.commands;
+
+import cback.TVBot;
+import cback.Util;
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.handle.obj.IGuild;
+import sx.blah.discord.handle.obj.IMessage;
+import sx.blah.discord.handle.obj.IRole;
+
+import java.util.List;
+
+public class CommandRoleID implements Command {
+    @Override
+    public String getName() {
+        return "roleid";
+    }
+
+    @Override
+    public void execute(TVBot bot, IDiscordClient client, String[] args, IGuild guild, IMessage message, boolean isPrivate) {
+        if (message.getAuthor().getID().equals("73416411443113984")) {
+            if (args.length == 1) {
+                String roleName = args[0];
+                List<IRole> serverRoles = guild.getRoles();
+                for (IRole roles : serverRoles) {
+                    if (roleName.equalsIgnoreCase("listall")) {
+                        Util.sendBufferedMessage(message.getChannel(), roles.getName() + " " + roles.getID());
+                    } else {
+                        if (roles.getName().equalsIgnoreCase(roleName)) {
+                            Util.sendMessage(message.getChannel(), "Found id for **" + roleName + "**: " + roles.getID());
+                        } else {
+                            Util.sendMessage(message.getChannel(), "Role not found");
+                        }
+                    }
+                }
+                Util.deleteMessage(message);
+            }
+        }
+    }
+
+    @Override
+    public boolean isLogged() {
+        return false;
+    }
+}
