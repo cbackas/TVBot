77d2147d92f522c444dddedd5602fc1c8b65fb94
cbackas <cbackasSE@gmail.com>
2016-10-14 19:41:13 -0500

    Maybe fixes movienight commands

--- src/main/java/cback/commands/CommandMovieNight.java
+++ src/main/java/cback/commands/CommandMovieNight.java
@@ -9,6 +9,8 @@ import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CommandMovieNight implements Command {
     @Override
@@ -19,47 +21,67 @@ public class CommandMovieNight implements Command {
     @Override
     public void execute(TVBot bot, IDiscordClient client, String[] args, IGuild guild, IMessage message, boolean isPrivate) {
         ConfigManager configManager = bot.getConfigManager();
-        String option = args[0];
+        String text = message.getContent();
         if (message.getAuthor().getRolesForGuild(guild).contains(guild.getRoleByID("226443478664609792"))) {
-            if (option.equalsIgnoreCase("set")) {
-                String poll = "<https://goo.gl/forms/" + args[1] + ">";
-                String date = StringUtils.join(" ", Arrays.copyOfRange(args, 2, args.length));
-                Util.deleteMessage(client.getMessageByID(configManager.getConfigValue("mnID")));
-                String announcement = "Polls for next weeks movie night is now open! " + poll + " Movie night will be on " + date + ".";
-                IMessage thingy = Util.sendBufferedMessage(guild.getChannelByID(TVBot.ANNOUNCEMENT_CHANNEL_ID), announcement);
-                String messageID = thingy.getID();
-                Util.sendBufferedMessage(guild.getChannelByID(TVBot.GENERAL_CHANNEL_ID), announcement);
-                try {
-                    configManager.setConfigValue("poll", poll);
-                    configManager.setConfigValue("date", date);
-                    configManager.setConfigValue("mnID", messageID);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } else if (option.equalsIgnoreCase("announce")) {
-                String movie = StringUtils.join(" ", Arrays.copyOfRange(args, 1, args.length));
-                String announcement = "The movie night poll is now closed! The movie that won is " + movie + ". Movie night will be on " + configManager.getConfigValue("date");
-                Util.deleteMessage(client.getMessageByID(configManager.getConfigValue("mnID")));
-                IMessage thingy = Util.sendBufferedMessage(guild.getChannelByID(TVBot.ANNOUNCEMENT_CHANNEL_ID), announcement);
-                String messageID = thingy.getID();
-                Util.sendBufferedMessage(guild.getChannelByID(TVBot.GENERAL_CHANNEL_ID), announcement);
-                try {
-                    configManager.setConfigValue("movie", movie);
-                    configManager.setConfigValue("mnID", messageID);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } else if (option.equalsIgnoreCase("start")) {
-                String rabbit = "<https://rabb.it/" + args[1] + ">";
-                String announcement = "We are about to start watching " + configManager.getConfigValue("movie") + "! Come check it out here " + rabbit;
-                Util.deleteMessage(client.getMessageByID(configManager.getConfigValue("mnID")));
-                IMessage thingy = Util.sendBufferedMessage(guild.getChannelByID(TVBot.ANNOUNCEMENT_CHANNEL_ID), announcement);
-                String messageID = thingy.getID();
-                Util.sendBufferedMessage(guild.getChannelByID(TVBot.GENERAL_CHANNEL_ID), announcement);
-                try {
-                    configManager.setConfigValue("mnID", messageID);
-                } catch (Exception e) {
-                    e.printStackTrace();
+            Pattern pattern = Pattern.compile("^!movienight (.+) ?.+?");
+            Matcher matcher = pattern.matcher(text);
+            if (matcher.find()) {
+                String option = matcher.group(1);
+                if (option.equalsIgnoreCase("set")) {
+                    Pattern patternSet = Pattern.compile("!movienight set (.+) (.+)");
+                    Matcher matcherSet = patternSet.matcher(text);
+                    if (matcher.find()) {
+                        String poll = "<https://goo.gl/forms/" + matcherSet.group(1) + ">";
+                        String date = matcherSet.group(2);
+                        if (date == null) {
+                            date = "unspecified date";
+                        }
+                        Util.deleteMessage(client.getMessageByID(configManager.getConfigValue("mnID")));
+                        String announcement = "Polls for next weeks movie night is now open! " + poll + " Movie night will be on " + date + ".";
+                        IMessage thingy = Util.sendBufferedMessage(guild.getChannelByID(TVBot.ANNOUNCEMENT_CHANNEL_ID), announcement);
+                        String messageID = thingy.getID();
+                        Util.sendBufferedMessage(guild.getChannelByID(TVBot.GENERAL_CHANNEL_ID), announcement);
+                        try {
+                            configManager.setConfigValue("poll", poll);
+                            configManager.setConfigValue("date", date);
+                            configManager.setConfigValue("mnID", messageID);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } else if (option.equalsIgnoreCase("announce")) {
+                    Pattern patternAnnounce = Pattern.compile("!movienight announce (.+)");
+                    Matcher matcherAnnounce = patternAnnounce.matcher(text);
+                    if (matcher.find()) {
+                        String movie = matcherAnnounce.group(1);
+                        String announcement = "The movie night poll is now closed! The movie that won is " + movie + ". Movie night will be on " + configManager.getConfigValue("date");
+                        Util.deleteMessage(client.getMessageByID(configManager.getConfigValue("mnID")));
+                        IMessage message2Delete = Util.sendBufferedMessage(guild.getChannelByID(TVBot.ANNOUNCEMENT_CHANNEL_ID), announcement);
+                        String messageID = message2Delete.getID();
+                        Util.sendBufferedMessage(guild.getChannelByID(TVBot.GENERAL_CHANNEL_ID), announcement);
+                        try {
+                            configManager.setConfigValue("movie", movie);
+                            configManager.setConfigValue("mnID", messageID);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } else if (option.equalsIgnoreCase("start")) {
+                    Pattern patternStart = Pattern.compile("!movienight start (.+)");
+                    Matcher matcherStart = patternStart.matcher(text);
+                    if (matcher.find()) {
+                        String rabbit = "<https://rabb.it/" + matcherStart.group(1) + ">";
+                        String announcement = "We are about to start watching " + configManager.getConfigValue("movie") + "! Come check it out here " + rabbit;
+                        Util.deleteMessage(client.getMessageByID(configManager.getConfigValue("mnID")));
+                        IMessage thingy = Util.sendBufferedMessage(guild.getChannelByID(TVBot.ANNOUNCEMENT_CHANNEL_ID), announcement);
+                        String messageID = thingy.getID();
+                        Util.sendBufferedMessage(guild.getChannelByID(TVBot.GENERAL_CHANNEL_ID), announcement);
+                        try {
+                            configManager.setConfigValue("mnID", messageID);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
             }
         }
