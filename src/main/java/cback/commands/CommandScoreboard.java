b0a14f8a351de130407027d21e9ab309f28dc05f
cbackas <cbackasSE@gmail.com>
2016-10-23 01:33:07 -0500

    Backbone stuff

---
+++ src/main/java/cback/commands/CommandScoreboard.java
@@ -0,0 +1,29 @@
+package cback.commands;
+
+import cback.TVBot;
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.handle.obj.IGuild;
+import sx.blah.discord.handle.obj.IMessage;
+
+import java.util.List;
+
+public class CommandScoreboard implements Command {
+    @Override
+    public String getName() {
+        return "scoreboard";
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return null;
+    }
+
+    @Override
+    public void execute(TVBot bot, IDiscordClient client, String[] args, IGuild guild, IMessage message, boolean isPrivate) {
+    }
+
+    @Override
+    public boolean isLogged() {
+        return false;
+    }
+}
