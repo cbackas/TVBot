b0b6829f6072291d685c317e63a4aea610795211
cbackas <cbackasSE@gmail.com>
2016-10-25 21:09:51 -0500

    Make !roleid more accepting of spaces

--- src/main/java/cback/commands/CommandRoleID.java
+++ src/main/java/cback/commands/CommandRoleID.java
@@ -7,8 +7,10 @@ import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.handle.obj.IRole;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CommandRoleID implements Command {
     @Override
@@ -25,7 +27,7 @@ public class CommandRoleID implements Command {
     public void execute(TVBot bot, IDiscordClient client, String[] args, IGuild guild, IMessage message, boolean isPrivate) {
         if (message.getAuthor().getRolesForGuild(guild).contains(guild.getRoleByID(TVBot.DEV_ROLE_ID))) {
             if (args.length == 1) {
-                String roleName = args[0];
+                String roleName = Arrays.stream(args).collect(Collectors.joining(" "));
                 List<IRole> serverRoles = guild.getRoles();
                 if (roleName.equalsIgnoreCase("listall")) {
                     String roleList = serverRoles.stream().map(role -> role.getName() + " " + role.getID()).reduce("",(a, b) -> a + b + "\n");
