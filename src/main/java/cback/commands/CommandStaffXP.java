2a7a8fda81574797b6c69fccbb5650686fd1a134
cbackas <cbackasSE@gmail.com>
2016-10-30 17:05:12 -0500

    Adds StaffXP

---
+++ src/main/java/cback/commands/CommandStaffXP.java
@@ -0,0 +1,30 @@
+package cback.commands;
+
+import cback.TVBot;
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.handle.obj.IGuild;
+import sx.blah.discord.handle.obj.IMessage;
+
+import java.util.List;
+
+public class CommandStaffXP implements Command {
+    @Override
+    public String getName() {
+        return "staffxp";
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return null;
+    }
+
+    @Override
+    public void execute(TVBot bot, IDiscordClient client, String[] args, IGuild guild, IMessage message, boolean isPrivate) {
+
+    }
+
+    @Override
+    public boolean isLogged() {
+        return true;
+    }
+}
