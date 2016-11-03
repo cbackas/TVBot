86dec4389111ecd022f7ae2e474a3123a5568806
Troy <troyhayes50@gmail.com>
2016-11-02 23:31:39 -0400

    Added sort command

---
+++ src/main/java/cback/commands/CommandSort.java
@@ -0,0 +1,98 @@
+package cback.commands;
+
+import cback.TVBot;
+import cback.Util;
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IGuild;
+import sx.blah.discord.handle.obj.IMessage;
+import sx.blah.discord.util.DiscordException;
+import sx.blah.discord.util.MissingPermissionsException;
+import sx.blah.discord.util.RequestBuffer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class CommandSort implements Command {
+
+    @Override
+    public String getName() {
+        return "sort";
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return null;
+    }
+
+    @Override
+    public void execute(TVBot bot, IDiscordClient client, String[] args, IGuild guild, IMessage message, boolean isPrivate) {
+
+        if (bot.getBotAdmins().contains(message.getAuthor().getID())) {
+
+            //permanent channels sorted by position to keep on top or bottom
+            List<IChannel> permChannels = bot.getConfigManager().getConfigArray("permanentchannels").stream()
+                    .map(id -> guild.getChannelByID(id))
+                    .sorted((chan1, chan2) -> Integer.compare(chan1.getPosition(), chan2.getPosition()))
+                    .collect(Collectors.toList());
+            List<IChannel> permChannelsTop = new ArrayList<>();
+            List<IChannel> permChannelsBottom = new ArrayList<>();
+            //add top and bottom channels to their respective lists
+            IntStream.range(0, permChannels.size())
+                    .forEach(index -> {
+                        IChannel channel = permChannels.get(index);
+                        if (channel.getPosition() == index)
+                            permChannelsTop.add(channel);
+                        else
+                            permChannelsBottom.add(channel);
+
+                    });
+
+            //sort non permanent channels
+            List<IChannel> showsChannelsSorted = guild.getChannels().stream()
+                    .filter(chan -> !permChannels.contains(chan))
+                    .sorted((chan1, chan2) -> getSortName(chan1.getName()).compareTo(getSortName(chan2.getName())))
+                    .collect(Collectors.toList());
+
+            //add newly sorted channels in order
+            List<IChannel> allChannelsSorted = new ArrayList<>();
+            allChannelsSorted.addAll(permChannelsTop);
+            allChannelsSorted.addAll(showsChannelsSorted);
+            allChannelsSorted.addAll(permChannelsBottom);
+
+            //apply new positions
+            IntStream.range(0, allChannelsSorted.size()).forEach(position -> {
+                RequestBuffer.request(() -> {
+                    try {
+                        allChannelsSorted.get(position).changePosition(position);
+                    } catch (DiscordException e) {
+                        e.printStackTrace();
+                    } catch (MissingPermissionsException e) {
+                        e.printStackTrace();
+                    }
+                });
+            });
+
+            Util.deleteMessage(message);
+        }
+    }
+
+    @Override
+    public boolean isLogged() {
+        return true;
+    }
+
+    public static String getSortName(String channelName) {
+        String newName = channelName.replaceAll("-", " ");
+        Matcher matcher = Pattern.compile("^(the|a) ").matcher(newName);
+        if (matcher.find()) {
+            newName = matcher.replaceFirst("");
+        }
+        return newName;
+    }
+
+}
--- src/main/java/cback/commands/CommandTrigger.java
+++ src/main/java/cback/commands/CommandTrigger.java
@@ -1,18 +1,11 @@
 package cback.commands;
 
 import cback.TVBot;
-import cback.Util;
 import sx.blah.discord.api.IDiscordClient;
-import sx.blah.discord.handle.obj.IChannel;
-import sx.blah.discord.handle.obj.IDiscordObject;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
-import sx.blah.discord.util.RequestBuffer;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class CommandTrigger implements Command {
     @Override
@@ -28,27 +21,7 @@ public class CommandTrigger implements Command {
     @Override
     public void execute(TVBot bot, IDiscordClient client, String[] args, IGuild guild, IMessage message, boolean isPrivate) {
         if (message.getAuthor().getID().equals("73416411443113984")) {
-            List<IChannel> permChannels = bot.getConfigManager().getConfigArray("permanentchannels").stream().map(id -> guild.getChannelByID(id)).collect(Collectors.toList());
-            List<IChannel> allChannels = guild.getChannels();
-            permChannels.forEach(allChannels::remove);
 
-            List<String> channelNames = new ArrayList<>();
-            channelNames.addAll(allChannels.stream().map(IChannel::getName).collect(Collectors.toList()));
-            channelNames.sort((p1, p2) -> p1.compareTo(p2));
-
-            for (String c : channelNames) {
-                int position = channelNames.indexOf(c) + 15;
-                RequestBuffer.request(() -> {
-                    try {
-                        guild.getChannelsByName(c).get(0).changePosition(position);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                });
-            }
-
-
-            Util.deleteMessage(message);
         }
     }
 
