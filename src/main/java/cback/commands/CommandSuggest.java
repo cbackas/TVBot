05ac6fb7648ee31767b81540ec161ac7e7069e48
cbackas <cbackasSE@gmail.com>
2016-12-12 16:40:09 -0600

    Adds movielounge suggestion channel to !suggest whitelist

--- src/main/java/cback/commands/CommandSuggest.java
+++ src/main/java/cback/commands/CommandSuggest.java
@@ -21,7 +21,7 @@ public class CommandSuggest implements Command {
 
     @Override
     public void execute(TVBot bot, IDiscordClient client, String[] args, IGuild guild, IMessage message, boolean isPrivate) {
-        if (message.getChannel().getID().equals("192444470942236672") || message.getChannel().getID().equals("214763749867913216")) {
+        if (message.getChannel().getID().equals("192444470942236672") || message.getChannel().getID().equals("214763749867913216") || message.getChannel().getID().equals("256491839870337024")) {
             try {
                 message.getChannel().pin(client.getMessageByID(message.getID()));
             } catch (Exception e) {
