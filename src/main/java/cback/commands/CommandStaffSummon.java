1250e465c2fd02c51122c5d131ecda4592fe633e
cbackas <cbackasSE@gmail.com>
2016-12-28 13:12:48 -0600

    change !staff to work with StaffHQ

--- src/main/java/cback/commands/CommandStaffSummon.java
+++ src/main/java/cback/commands/CommandStaffSummon.java
@@ -27,7 +27,7 @@ public class CommandStaffSummon implements Command {
         //Lounge Command Only
         if (guild.getID().equals("192441520178200577")) {
             if (!bot.getConfigManager().getConfigArray("cantsummon").contains(message.getAuthor().getID())) {
-                IChannel modLounge = client.getChannelByID("192444648545845248");
+                IChannel staffHQ = client.getChannelByID("226433456060497920");
                 List<IUser> mods = Util.getUsersByRole(TVRoles.MOD.id);
 
                 StringBuilder modMentions = new StringBuilder();
@@ -36,7 +36,7 @@ public class CommandStaffSummon implements Command {
                 }
 
                 Util.sendMessage(message.getChannel(), "Staff have been notified and will come shortly.");
-                Util.sendMessage(modLounge, message.getAuthor().mention() + " has requested staff in " + message.getChannel().mention() + "\n" + modMentions.toString());
+                Util.sendMessage(staffHQ, message.getAuthor().mention() + " has requested staff in " + message.getChannel().mention() + "\n" + modMentions.toString());
 
                 Util.botLog(message);
                 Util.deleteMessage(message);
