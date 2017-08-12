f10f3f099a2549c293f78415d3c94bf140a951a1
cbackas <cbackasSE@gmail.com>
2017-08-12 17:55:47 -0500

    Update rules with titles and new message IDs

--- src/main/java/cback/Rules.java
+++ src/main/java/cback/Rules.java
@@ -1,18 +1,20 @@
 package cback;
 
 public enum Rules {
-    one("1", Util.getRule("263187086595719171")),
-    two("2", Util.getRule("263187123769704458")),
-    three("3", Util.getRule("263187157361885194")),
-    four("4", Util.getRule("263187200550764545")),
-    five("5", Util.getRule("263187237397594113")),
-    six("6", Util.getRule("263187288152866817")),
-    other("other", Util.getRule("263187326061117442"));
+    one("1", "Rule One | Civil Discussion", Util.getRule("345792703046615043")),
+    two("2", "Rule Two | Spam**", Util.getRule("345792898371026945")),
+    three("3", "Rule Three | Self-Promotion", Util.getRule("345792952255250442")),
+    four("4", "Rule Four | Spoilers", Util.getRule("345793033171894272")),
+    five("5", "Rule Five | NSFW Content", Util.getRule("345793123261349889")),
+    six("6", "Rule Six | Bots and Exploits", Util.getRule("345793187861757953")),
+    other("other", "Other", Util.getRule("345793249933525022"));
 
     public String number;
+    public String title;
     public String fullRule;
 
-    Rules(String number, String fullRule) {
+    Rules(String number, String title, String fullRule) {
+        this.title = title;
         this.number = number;
         this.fullRule = fullRule;
     }
