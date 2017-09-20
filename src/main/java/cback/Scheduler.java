8bbcdb3db9203f61d9d75ebee93c315102b51a23
cbackas <cbackasSE@gmail.com>
2017-09-20 18:58:19 -0500

    Netflix/amazon shows can be announced now - it probably won't spam

--- src/main/java/cback/Scheduler.java
+++ src/main/java/cback/Scheduler.java
@@ -5,7 +5,11 @@ import cback.database.tv.Show;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
+import sx.blah.discord.util.EmbedBuilder;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -65,7 +69,7 @@ public class Scheduler {
 
         //update user count at midnight every night
         int currentTimeEST = time - 14400; //EST time, subtract 4 hours from UTC
-        int midnightWaitTime = roundUp(currentTimeEST, DAILY_INTERVAL) - currentTimeEST; //seconds until midnight
+        int midnightWaitTime = roundUp(currentTimeEST, DAILY_INTERVAL) - currentTimeEST + 45; //seconds until midnight
         exec.scheduleAtFixedRate(() -> {
 
             /**
@@ -73,6 +77,7 @@ public class Scheduler {
              */
             updateUserCount();
             resetUserChange();
+            sendDailyMessage();
 
         }, midnightWaitTime, DAILY_INTERVAL, TimeUnit.SECONDS);
     }
@@ -83,24 +88,35 @@ public class Scheduler {
     public void processNewAirings(int currentTime) {
         //get next 30 shows airing
         List<Airing> nextAirings = bot.getDatabaseManager().getTV().getNewAirings();
+        List<String> bulkShowIDs = new ArrayList<>();
         //if it airs in next 11 minutes, send message and update in database
         nextAirings.stream().filter(airing -> airing.getAiringTime() - currentTime <= ALERT_TIME_THRESHOLD).forEach(airing -> {
             try {
                 Show show = bot.getDatabaseManager().getTV().getShow(airing.getShowID());
                 if (show != null) {
-                    IChannel announceChannel = bot.getClient().getChannelByID(TVBot.NEWEPISODE_CH_ID);
-                    IChannel globalChannel = bot.getClient().getChannelByID(TVBot.GENERAL_CH_ID);
-                    IChannel showChannel = bot.getClient().getChannelByID(Long.parseLong(show.getChannelID()));
+                    TraktManager tm = bot.getTraktManager();
+                    String network = tm.showSummary(show.getShowID()).network;
+                    if (network.equalsIgnoreCase("netflix") || network.equalsIgnoreCase("amazon")) {
+                        if (bulkShowIDs.contains(show.getShowID())) {
+                            return;
+                        } else {
+                            bulkShowIDs.add(show.getShowID());
 
-                    String message = "**" + show.getShowName() + " " + airing.getEpisodeInfo() + "** is about to start on " + bot.getTraktManager().showSummary(show.getShowID()).network + ". Go to " + showChannel.mention() + " for live episode discussion!";
+                            IChannel announceChannel = bot.getClient().getChannelByID(TVBot.NEWEPISODE_CH_ID);
+                            IChannel globalChannel = bot.getClient().getChannelByID(TVBot.GENERAL_CH_ID);
+                            IChannel showChannel = bot.getClient().getChannelByID(Long.parseLong(show.getChannelID()));
 
-                    IMessage announceMessage = Util.sendBufferedMessage(announceChannel, message);
-                    airing.setMessageID(announceMessage.getStringID());
-                    bot.getDatabaseManager().getTV().updateAiringMessage(airing);
-                    Util.sendBufferedMessage(globalChannel, message);
-                    Util.sendBufferedMessage(showChannel, "**" + show.getShowName() + " " + airing.getEpisodeInfo() + "** is about to start.");
+                            String message = "**" + show.getShowName() + " " + airing.getEpisodeInfo() + "** is about to start on " + tm.showSummary(show.getShowID()).network + ". Go to " + showChannel.mention() + " for live episode discussion!";
 
-                    System.out.println("Sent announcement for " + airing.getEpisodeInfo());
+                            IMessage announceMessage = Util.sendBufferedMessage(announceChannel, message);
+                            airing.setMessageID(announceMessage.getStringID());
+                            bot.getDatabaseManager().getTV().updateAiringMessage(airing);
+                            Util.sendBufferedMessage(globalChannel, message);
+                            Util.sendBufferedMessage(showChannel, "**" + show.getShowName() + " " + airing.getEpisodeInfo() + "** is about to start.");
+
+                            System.out.println("Sent announcement for " + airing.getEpisodeInfo());
+                        }
+                    }
                 } else {
                     System.out.println("Tried to announce airing for unsaved show, deleting airing...");
                     bot.getDatabaseManager().getTV().deleteAiring(airing.getEpisodeID());
@@ -177,5 +193,33 @@ public class Scheduler {
     public static int roundUp(double i, int v) {
         Double rounded = Math.ceil(i / v) * v;
         return rounded.intValue();
+    }
+
+    /**
+     * Sends a message in the new show channel daily to separate days
+     */
+    public void sendDailyMessage() {
+        EmbedBuilder embed = new EmbedBuilder();
+
+        LocalDate now = LocalDate.now();
+        String dayOfWeek = now.getDayOfWeek().name();
+        String month = now.getMonth().name();
+        int day = now.getDayOfMonth();
+
+        List<Airing> nextAirings = bot.getDatabaseManager().getTV().getNewAirings();
+        //if it airs in next 24 hours it goes on the count
+        long count = nextAirings.stream().filter(airing -> airing.getAiringTime() - Util.getCurrentTime() <= DAILY_INTERVAL).count();
+
+        embed
+                .withTitle(dayOfWeek + ", " + month + " " + day)
+                .withColor(Util.BOT_COLOR);
+
+        if (count > 0) {
+            embed.withDesc("There are " + count + " episodes airing today! Stick around to see what they are.");
+        } else {
+            embed.withDesc("There aren't any new episodes airing today. Maybe tomorrow will be interesting.");
+        }
+
+        Util.sendEmbed(bot.getClient().getChannelByID(TVBot.DEV_CH_ID), embed.build());
     }
 }
