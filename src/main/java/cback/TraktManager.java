9cb66e7a4ce5663ffc5d213832bf37262473e936
cbackas <cbackasSE@gmail.com>
2017-10-06 20:29:02 -0500

    adsf

--- src/main/java/cback/TraktManager.java
+++ src/main/java/cback/TraktManager.java
@@ -13,7 +13,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 public class TraktManager {
 
