db9e6ee42b803cbd02b1fd582faf88a93112a579
cbackas <cbackasSE@gmail.com>
2017-10-09 14:12:52 -0500

    New show searching - should be better

--- src/main/java/cback/TraktManager.java
+++ src/main/java/cback/TraktManager.java
@@ -7,8 +7,15 @@ import com.uwetrottmann.tmdb2.services.SearchService;
 import com.uwetrottmann.trakt5.TraktV2;
 import com.uwetrottmann.trakt5.entities.*;
 import com.uwetrottmann.trakt5.enums.Extended;
+import com.uwetrottmann.trakt5.enums.IdType;
+import com.uwetrottmann.trakt5.enums.Type;
 import retrofit2.Response;
 
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 public class TraktManager {
@@ -43,9 +50,50 @@ public class TraktManager {
         tmdb = new Tmdb(tmdbToken.get());
     }
 
-    public Show showSummaryFromName(String showName) {
+    public void updateAiringData() {
         try {
-            Response<Show> show = trakt.shows().summary(searchTmdbShow(showName), Extended.FULL).execute();
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = new Date();
+            Response<List<CalendarShowEntry>> response = trakt.calendars().shows(dateFormat.format(date), 3).execute();
+            if (response.isSuccessful()) {
+                List<CalendarShowEntry> shows = response.body();
+                List<String> desiredShows = bot.getDatabaseManager().getTV().getShowIDs();
+                for (CalendarShowEntry entry : shows) {
+                    String id = entry.show.ids.imdb;
+                    if (desiredShows.contains(id)) {
+                        //int airTime = Util.toInt(entry.first_aired.toMillis() / 1000);
+                        int airTime = Math.toIntExact(entry.first_aired.toInstant().toEpochMilli() / 1000);
+                        int currentTime = Util.getCurrentTime();
+                        String episodeID = String.valueOf(entry.episode.ids.trakt);
+                        //don't add if already aired or if airing already in database
+                        if ((bot.getDatabaseManager().getTV().getAiring(episodeID) == null) && (airTime > currentTime)) {
+                            String episodeInfo = "S" + entry.episode.season + "E" + entry.episode.number + " - " + entry.episode.title;
+                            bot.getDatabaseManager().getTV().insertAiring(episodeID, id, airTime, episodeInfo, "NONE");
+                            System.out.println("Found Show Airing: " + entry.show.title + ": " + episodeInfo + " - " + airTime);
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            Util.reportHome(e);
+        }
+    }
+
+    public String getShowTitle(String imdbID) {
+        try {
+            //Response<List<SearchResult>> search = trakt.search().idLookup(IdType.IMDB, imdbID, 1, 1).execute();
+            Response<List<SearchResult>> search = trakt.search().idLookup(IdType.IMDB, imdbID, Type.SHOW, Extended.NOSEASONS, 1, 1).execute();
+            if (search.isSuccessful() && !search.body().isEmpty()) {
+                return search.body().get(0).show.title;
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public Show showSummary(String imdbID) {
+        try {
+            Response<Show> show = trakt.shows().summary(imdbID, Extended.FULL).execute();
             if (show.isSuccessful()) {
                 return show.body();
             }
@@ -54,11 +102,22 @@ public class TraktManager {
         return null;
     }
 
+    public Show showSummaryFromName(String showName) {
+        try {
+            Response<List<SearchResult>> search = trakt.search().idLookup(IdType.IMDB, searchTmdbShow(showName), Type.SHOW, Extended.FULL, 1, 1).execute();
+            if (search.isSuccessful()) {
+                return search.body().get(0).show;
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
     public Movie movieSummaryFromName(String movieName) {
         try {
-            Response<Movie> movie = trakt.movies().summary(searchTmdbMovie(movieName), Extended.FULL).execute();
-            if (movie.isSuccessful()) {
-                return movie.body();
+            Response<List<SearchResult>> search = trakt.search().idLookup(IdType.IMDB, searchTmdbMovie(movieName), Type.MOVIE, Extended.FULL, 1, 1).execute();
+            if (search.isSuccessful()) {
+                return search.body().get(0).movie;
             }
         } catch (Exception e) {
         }
