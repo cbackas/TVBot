bd8ef1df991efa451e1afe34a763b56ef117d957
Zac Gibson <zgibson@live.com>
2021-08-01 04:07:17 -0500

    refactor and start debugging booster list

--- src/main/java/cback/commandsV2/CommandInfo.java
+++ src/main/java/cback/commandsV2/CommandInfo.java
@@ -29,21 +29,21 @@ public class CommandInfo extends Command {
                 .setEphemeral(true)
                 .flatMap(v -> {
                     int userCount = event.getGuild().getMemberCount();
-                    List<Member> boostCount = event.getGuild().getBoosters();
+                    List<Member> boosters = event.getGuild().getBoosters();
+                    System.out.println(boosters);
                     int channelCount = event.getGuild().getChannels().size();
                     OffsetDateTime serverCreatedDateTime = event.getGuild().getTimeCreated();
                     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd, yyyy");
 
-                    EmbedBuilder embed = new EmbedBuilder();
-                    embed.setTitle(event.getGuild().getName());
-                    embed.addField("Created: ", serverCreatedDateTime.format(formatter), true);
-                    embed.addField("Members: ", Integer.toString(userCount), true);
-                    embed.addField("Channels: ", String.valueOf(channelCount), true);
-                    embed.addField("Server Boosters: ", boostCount.stream().map(Member::getNickname).collect(Collectors.joining("\n")), true);
-                    embed.addBlankField(false);
-                    embed.addField("Source Code: ", "[`GitHub`](https://github.com/cbackas/TVBot)", true);
-
-                    MessageEmbed infoEmbed = embed.setColor(Util.getBotColor())
+                    MessageEmbed infoEmbed = new EmbedBuilder()
+                            .setColor(Util.getBotColor())
+                            .setTitle(event.getGuild().getName())
+                            .setThumbnail(event.getGuild().getIconUrl())
+                            .addField("Created: ", serverCreatedDateTime.format(formatter), true)
+                            .addField("Members: ", Integer.toString(userCount), true)
+                            .addField("Channels: ", String.valueOf(channelCount), true)
+                            .addField("Server Boosters: ", boosters.stream().map(Member::getNickname).collect(Collectors.joining("\n")), true)
+                            .addField("Source Code: ", "[`GitHub`](https://github.com/cbackas/TVBot)", true)
                             .build();
 
                     return event.getHook().editOriginalEmbeds(infoEmbed);
