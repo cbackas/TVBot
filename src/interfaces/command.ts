e0895c86d93afca12be4d29bdb4020ef087475d0
cbackas <zgibson@live.com>
2022-12-17 22:32:15 -0600

    make command type more flexible

--- src/interfaces/command.ts
+++ src/interfaces/command.ts
@@ -1,6 +1,6 @@
-import { CacheType, ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js"
+import { CacheType, ChatInputCommandInteraction, Message, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "discord.js"
 
-export type Command = {
-    data: SlashCommandBuilder
-    execute(interaction: ChatInputCommandInteraction<CacheType>): Promise<void>
+export type Command<> = {
+    data: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder
+    execute(interaction: ChatInputCommandInteraction<CacheType>): Promise<void | Message<boolean>>
 }
