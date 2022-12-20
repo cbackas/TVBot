38b297878d39075278e1e124d0c7e979d8557a34
cbackas <zgibson@live.com>
2022-12-19 22:48:46 -0600

    update interface

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,11 +1,12 @@
 import { CacheType, ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js'
+import { App } from '../app'
 import { Command } from '../interfaces/command'
 
 const command: Command = {
   data: new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Replies with Pong!'),
-  async execute(interaction: ChatInputCommandInteraction<CacheType>) {
+  async execute(_app: App, interaction: ChatInputCommandInteraction<CacheType>) {
     await interaction.reply('Pong!')
   }
 }
