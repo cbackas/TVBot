f1acc874aa27d5911f5eaac0d8efc0ee3403943a
cbackas <zgibson@live.com>
2022-12-22 00:24:22 -0600

    support more slash command builders in command interface

--- src/interfaces/command.ts
+++ src/interfaces/command.ts
@@ -1,9 +1,9 @@
-import { CacheType, ChatInputCommandInteraction, InteractionResponse, Message, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "discord.js"
+import { CacheType, ChatInputCommandInteraction, InteractionResponse, Message, SlashCommandBuilder, SlashCommandOptionsOnlyBuilder, SlashCommandSubcommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "discord.js"
 import { App } from "../app"
 
 type ExecuteFunction = Promise<void | Message<boolean> | InteractionResponse<boolean>>
 
 export type Command<> = {
-  data: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder
+  data: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">
   execute(app: App, interaction: ChatInputCommandInteraction<CacheType>): ExecuteFunction
 }
