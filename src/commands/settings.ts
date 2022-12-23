8421cce4762cb208e026dc83026e51217bf96dfd
cbackas <zgibson@live.com>
2022-12-22 20:04:18 -0600

    add all_episodes slash command data

--- src/commands/settings.ts
+++ src/commands/settings.ts
@@ -1,11 +1,11 @@
-import { APIInteractionDataResolvedChannel, CacheType, ChannelType, ChatInputCommandInteraction, ForumChannel, PermissionFlagsBits, SlashCommandBuilder, SlashCommandSubcommandBuilder } from 'discord.js'
+import { APIInteractionDataResolvedChannel, CacheType, ChannelType, ChatInputCommandInteraction, ForumChannel, PermissionFlagsBits, SlashCommandBuilder, SlashCommandSubcommandBuilder, SlashCommandSubcommandGroupBuilder } from 'discord.js'
 import client from '../lib/prisma'
 import { Command } from '../interfaces/command'
 import { ProgressMessageBuilder } from '../lib/progressMessages'
 import { App } from '../app'
 
 const slashCommand = new SlashCommandBuilder()
-  .setName('settings')
+  .setName('setting')
   .setDescription('Configura various bot settings')
   .setDMPermission(false)
   .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
@@ -14,13 +14,27 @@ const slashCommand = new SlashCommandBuilder()
     .setDescription('Set the forum ID for TV shows')
     .addChannelOption(option => option.setName('channel')
       .setDescription('The channel to set as the TV forum')
-      .addChannelTypes(ChannelType.GuildForum)
-    )
-  )
+      .addChannelTypes(ChannelType.GuildForum)))
+  .addSubcommandGroup(new SlashCommandSubcommandGroupBuilder()
+    .setName('all_episodes')
+    .setDescription('Add or remove a channel from the list that receive all episode notifications')
+    .addSubcommand(new SlashCommandSubcommandBuilder()
+      .setName('add')
+      .setDescription('Add a channel from the list that receive all episode notifications')
+      .addChannelOption(option => option.setName('channel')
+        .setDescription('Channel to add to the list that recieves all episode notifications')
+        .addChannelTypes(ChannelType.GuildText)
+        .setRequired(true)))
+    .addSubcommand(new SlashCommandSubcommandBuilder()
+      .setName('remove')
+      .setDescription('Remove a channel from the list that receive all episode notifications')
+      .addChannelOption(option => option.setName('channel')
+        .setDescription('Channel to remove from the list that recieves all episode notifications')
+        .addChannelTypes(ChannelType.GuildText)
+        .setRequired(true))))
 
 const execute = async (app: App, interaction: ChatInputCommandInteraction<CacheType>) => {
   const subCommand = interaction.options.getSubcommand()
-  if (!subCommand) return await interaction.editReply('Invalid subcommand')
 
   if (subCommand === 'tv_forum') {
     const channel = interaction.options.getChannel('channel', true)
@@ -30,9 +44,10 @@ const execute = async (app: App, interaction: ChatInputCommandInteraction<CacheT
     }
 
     await setTVForum(interaction, channel)
+    await app.loadSettings()
   }
 
-  return await app.loadSettings()
+  return await interaction.editReply('Invalid subcommand')
 }
 
 export const command: Command = {
