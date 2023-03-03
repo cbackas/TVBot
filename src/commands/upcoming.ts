02fcbab5daeac1b6b87b7540ce59105c8179a180
cbackas <zgibson@live.com>
2023-03-02 22:43:32 -0600

    use new upcoming grouping in upcoming command and impl /upcoming all

--- src/commands/upcoming.ts
+++ src/commands/upcoming.ts
@@ -1,12 +1,12 @@
-import { type APIEmbed, type APIEmbedField, type AutocompleteInteraction, type ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandSubcommandBuilder } from 'discord.js'
+import { type AutocompleteInteraction, type ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandSubcommandBuilder } from 'discord.js'
 import client from '../lib/prisma'
 import { type CommandV2 } from '../interfaces/command'
 import { type App } from '../app'
 import { getSeriesByImdbId } from '../lib/tvdb'
 import { showSearchAutocomplete } from '../lib/autocomplete'
-import moment from 'moment'
-import { addLeadingZeros } from '../lib/util'
 import { type Show } from '@prisma/client'
+import { ProgressError } from '../interfaces/error'
+import { getUpcomingEpisodesEmbed } from '../lib/upcoming'
 
 export const command: CommandV2 = {
   slashCommand: {
@@ -32,115 +32,92 @@ export const command: CommandV2 = {
   async executeCommand (_app: App, interaction: ChatInputCommandInteraction) {
     const subCommand = interaction.options.getSubcommand()
 
-    let s: Show | Show[] | undefined
+    let s: Show[] = []
 
-    if (subCommand === 'here') {
-      /**
-       * Get all shows that are linked to this channel
-       */
-      const shows = await client.show.findMany({
-        where: {
-          destinations: {
-            some: {
-              channelId: interaction.channelId
-            }
-          }
-        }
-      })
-
-      if (shows.length === 0) return await interaction.editReply('No shows linked to this channel')
-
-      s = shows
-    } else {
-      /**
-       * Get the show by IMDB ID in the query
-       */
-      const query = interaction.options.getString('query', true)
-
-      // check that the query is an IMDB ID
-      const imdbId = query.toLowerCase().startsWith('tt') ? query : undefined
-      if (imdbId == null) return await interaction.editReply('Invalid query')
-
-      // turn IMDB ID into a series
-      const series = await getSeriesByImdbId(imdbId)
-      if (series === undefined) return await interaction.editReply(`No show found with IMDB ID \`${imdbId}\``)
-
-      const show = await client.show.findUnique({
-        where: {
-          imdbId
-        }
-      })
-
-      if (show == null) return await interaction.editReply(`${series.name} is not linked to any channels. Use \`/link\` or \`/post\` to subscribe a channel to episode notifications.`)
-
-      s = show
+    try {
+      switch (subCommand) {
+        case 'all':
+          s = await getAllShows()
+          break
+        case 'here':
+          s = await getShowsHere(interaction.channelId)
+          break
+        case 'show':
+          s = [await getShowByImdbId(interaction.options.getString('query', true))]
+          break
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        return await interaction.editReply(error.message)
+      }
     }
 
-    // build embed with upcoming episodes as fields
-    const embed: APIEmbed = {
-      title: 'Upcoming episodes',
-      fields: createEmbedFields(s)
-    }
+    if (s == null || s.length === 0) return await interaction.editReply('No shows found')
 
-    return await interaction.editReply({ content: '', embeds: [embed] })
+    // const message = await getUpcomingEpisodesMessage(s, 7)
+    const embed = await getUpcomingEpisodesEmbed(s, 7)
+
+    return await interaction.editReply({
+      content: '',
+      embeds: [embed]
+    })
   },
   async executeAutoComplate (_app: App, interaction: AutocompleteInteraction) {
     await showSearchAutocomplete(interaction)
   }
 }
 
-/**
- * Converts a show or list of shows into a list of embed fields
- * Each field contains the name of the show and a list of upcoming episodes
- * @param s Show or array of shows
- * @returns Array of embed fields for shows
- */
-function createEmbedFields (s: Show | Show[]): APIEmbedField[] {
-  const shows = Array.isArray(s) ? s : [s]
-
-  // sort the shows by the air date of the first episode
-  const showsSorted = shows.sort((a, b) => {
-    // sort a show episodes by air date
-    const aEpisodes = a.episodes.sort((a, b) => {
-      const aDate = moment.utc(a.airDate)
-      const bDate = moment.utc(b.airDate)
-      return aDate.isBefore(bDate) ? -1 : 1
-    })
-
-    // sort b show episodes by air date
-    const bEpisodes = b.episodes.sort((a, b) => {
-      const aDate = moment.utc(a.airDate)
-      const bDate = moment.utc(b.airDate)
-      return aDate.isBefore(bDate) ? -1 : 1
-    })
-
-    return moment.utc(aEpisodes[0].airDate).isBefore(moment.utc(bEpisodes[0].airDate)) ? -1 : 1
-  })
-
-  // create embed fields from shows
-  const fields: APIEmbedField[] = showsSorted.map(show => {
-    return {
-      name: show.name,
-      value: show.episodes
-        // filter out episodes that have already aired
-        .filter(e => {
-          const momentUTC = moment.utc(new Date())
-          const airDateUTC = moment.utc(e.airDate)
-          return airDateUTC.isAfter(momentUTC)
-        })
-        // sort episodes by air date
-        .sort((a, b) => {
-          const aDate = moment.utc(a.airDate)
-          const bDate = moment.utc(b.airDate)
-          return aDate.isBefore(bDate) ? -1 : 1
-        })
-        // map episodes to strings
-        .map(e => {
-          return `S${addLeadingZeros(e.season, 2)}E${addLeadingZeros(e.number, 2)} ${e.title} - <t:${moment.utc(e.airDate).unix()}:R>`
-        })
-        .join('\n')
+async function getAllShows (): Promise<Show[]> {
+  const shows = await client.show.findMany({
+    where: {
+      episodes: {
+        some: { messageSent: false }
+      }
     }
   })
 
-  return fields
+  if (shows.length === 0) throw new ProgressError('No shows found')
+
+  return shows
+}
+
+/**
+ * Get all shows that are linked to this channel
+ */
+async function getShowsHere (channelId: string): Promise<Show[]> {
+  const shows = await client.show.findMany({
+    where: {
+      destinations: {
+        some: { channelId }
+      }
+    }
+  })
+
+  if (shows.length === 0) throw new ProgressError('No shows linked to this channel')
+
+  return shows
+}
+
+/**
+ * Get the show by IMDB ID in the querys
+ */
+async function getShowByImdbId (query: string): Promise<Show> {
+  // check that the query is an IMDB ID
+  const imdbId = query.toLowerCase().startsWith('tt') ? query : undefined
+  if (imdbId == null) throw new ProgressError('Invalid query')
+
+  // turn IMDB ID into a series
+  const series = await getSeriesByImdbId(imdbId)
+  // if (series == null) return await interaction.editReply(`No show found with IMDB ID \`${imdbId}\``)
+  if (series == null) throw new ProgressError(`No show found with IMDB ID \`${imdbId}\``)
+
+  const show = await client.show.findUnique({
+    where: {
+      imdbId
+    }
+  })
+
+  if (show == null) throw new ProgressError(`${series.name} is not linked to any channels. Use \`/link\` or \`/post\` to subscribe a channel to episode notifications.`)
+
+  return show
 }
