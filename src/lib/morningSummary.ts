ccc007321597b35c0a408527b46c68ab6634879c
cbackas <zgibson@live.com>
2023-03-02 22:43:50 -0600

    make the morning summary use the new embed thingy

--- src/lib/morningSummary.ts
+++ src/lib/morningSummary.ts
@@ -1,14 +1,29 @@
-import { ChannelType, type Client } from 'discord.js'
+import { type APIEmbed, ChannelType, type Client } from 'discord.js'
 import { type Settings } from './settingsManager'
-import { getUpcomingEpisodes } from './upcoming'
+import { getUpcomingEpisodesEmbed } from './upcoming'
+import client from './prisma'
+import { type Show } from '@prisma/client'
 
 export async function sendMorningSummary (settings: Settings, c: Client): Promise<void> {
-  const message = await getUpcomingEpisodes(1)
+  const shows: Show[] = await client.show.findMany({
+    where: {
+      episodes: {
+        some: {
+          messageSent: false
+        }
+      }
+    }
+  })
+
+  const embed: APIEmbed = await getUpcomingEpisodesEmbed(shows, 1)
 
   for (const dest of settings.morningSummaryDestinations) {
     const channel = await c.channels.fetch(dest.channelId)
     if ((channel == null) || !channel.isTextBased() || channel.type === ChannelType.GuildStageVoice) continue
 
-    await channel.send(message)
+    await channel.send({
+      content: '',
+      embeds: [embed]
+    })
   }
 }
