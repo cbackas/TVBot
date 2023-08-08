1c97930a832233d93e9b429dd5eb0fc28f14adac
Zac <zgibson@live.com>
2023-08-08 16:40:23 -0500

    Fix: tvdb is sending unexpected data, so lets handle it i guess (#17)

--- src/lib/tvdb.ts
+++ src/lib/tvdb.ts
@@ -32,14 +32,22 @@ export async function getSeriesByImdbId (imdbId: string): Promise<SeriesExtended
   const options = await axiosOptions()
 
   const response = await axios.get<{
-    data: SearchByRemoteIdResult[]
+    data?: SearchByRemoteIdResult[]
   }>(`/search/remoteid/${imdbId}`, options)
 
-  const data = response.data.data[0]
-  const series = data?.series
-  if (data == null || series == null || series.id == null) return undefined
+  if (response.status !== 200) {
+    console.error(JSON.stringify(response.data))
+    return undefined
+  }
 
-  return await getSeries(series.id)
+  const data = response.data.data?.at(0)
+  if (data == null) {
+    return undefined
+  } else if (data?.series?.id != null) {
+    return await getSeries(data?.series.id)
+  } else if (data?.season?.seriesId != null) {
+    return await getSeries(data?.season?.seriesId)
+  }
 }
 
 export async function getSeries (tvdbId: number): Promise<SeriesExtendedRecord | undefined> {
