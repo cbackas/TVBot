f46bc2daf9900b436355b0398cc5f2b1481427e7
Zac <zgibson@live.com>
2024-10-12 20:41:04 -0500

    Fix: semantic release wasn't releasing (#32)

--- release.config.cjs
+++ release.config.cjs
@@ -1,43 +1,16 @@
-/* eslint-disable indent */
 /* eslint-disable no-template-curly-in-string */
 /**
  * @typedef {import('semantic-release').Options} Options
- */
+*/
 
-const releaseRules = [
-  { tag: 'Breaking', release: 'major' },
-  { tag: 'Fix', release: 'patch' },
-  { tag: 'Update', release: 'minor' },
-  { tag: 'New', release: 'minor' },
-  { tag: 'Build', release: 'patch' },
-  { tag: 'Upgrade', release: 'patch' },
-  { tag: 'Chore', release: 'patch' }
-]
-
-const noteKeywords = releaseRules.map(rule => rule.tag)
 /** @type {Options} */
-
 module.exports = {
   branches: ['main'],
   tagFormat: '${version}',
   repositoryUrl: 'https://github.com/cbackas/TVBot',
   plugins: [
-    [
-      '@semantic-release/commit-analyzer',
-      {
-        preset: 'eslint',
-        releaseRules
-      }
-    ],
-    [
-      '@semantic-release/release-notes-generator',
-      {
-        preset: 'eslint',
-        parserOpts: {
-          noteKeywords
-        }
-      }
-    ],
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     [
       '@semantic-release/github',
       {
@@ -48,15 +21,16 @@ module.exports = {
       }
     ],
     [
-        '@codedependant/semantic-release-docker',
-        {
-          dockerTags: ['{{version}}', 'latest'],
-          dockerFile: 'Dockerfile',
-          dockerPlatform: ['linux/amd64', 'linux/arm64'],
-          dockerRegistry: 'ghcr.io',
-          dockerProject: 'cbackas',
-          dockerImage: 'tvbot'
-        }
-      ]
-  ]
+      '@codedependant/semantic-release-docker',
+      {
+        dockerTags: ['{{version}}', 'latest'],
+        dockerFile: 'Dockerfile',
+        dockerPlatform: ['linux/amd64', 'linux/arm64'],
+        dockerRegistry: 'ghcr.io',
+        dockerProject: 'cbackas',
+        dockerImage: 'tvbot'
+      }
+    ]
+  ],
+  preset: 'eslint'
 }
