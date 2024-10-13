95178fa914048d7bb6ceb4b17154cf2b203ab7fd
cbackas <zgibson@live.com>
2024-10-12 20:37:31 -0500

    changed semantic-release config, idk what i did but hopefully it works

--- release.config.cjs
+++ release.config.cjs
@@ -1,43 +1,16 @@
-/* eslint-disable indent */
 /* eslint-disable no-template-curly-in-string */
 /**
  * @typedef {import('semantic-release').Options} Options
 */
 
-const releaseRules = [
-  { tag: 'Breaking', release: 'major' },
-  { tag: 'Fix', release: 'patch' },
-  { tag: 'Update', release: 'minor' },
-  { tag: 'New', release: 'minor' },
-  { tag: 'Build', release: 'patch' },
-  { tag: 'Upgrade', release: 'patch' },
-  { tag: 'Chore', release: 'patch' }
-]
-
-const noteKeywords = releaseRules.map(rule => rule.tag)
 /** @type {Options} */
-
 module.exports = {
   branches: ['main'],
   tagFormat: '${version}',
   repositoryUrl: 'https://github.com/cbackas/TVBot',
   plugins: [
-    [
-      '@semantic-release/commit-analyzer',
-      {
-        preset: 'eslint',
-        releaseRules
-      }
-    ],
-    [
-      '@semantic-release/release-notes-generator',
-      {
-        preset: 'eslint',
-        parserOpts: {
-          noteKeywords
-        }
-      }
-    ],
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     [
       '@semantic-release/github',
       {
@@ -58,5 +31,10 @@ module.exports = {
         dockerImage: 'tvbot'
       }
     ]
+<<<<<<< Updated upstream
   ]
+=======
+  ],
+  preset: 'eslint'
+>>>>>>> Stashed changes
 }
