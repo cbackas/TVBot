38ebc0634d3e29f68c6fc367df0b662c40fd36d7
cbackas <zgibson@live.com>
2024-11-16 00:04:14 -0600

    rework the github actions

--- release.config.cjs
+++ release.config.cjs
@@ -1,36 +1,25 @@
 /* eslint-disable no-template-curly-in-string */
 /**
- * @typedef {import('semantic-release').Options} Options
-*/
+ * @typedef {import('npm:semantic-release').Options} Options
+ */
 
 /** @type {Options} */
 module.exports = {
-  branches: ['main'],
-  tagFormat: '${version}',
-  repositoryUrl: 'https://github.com/cbackas/TVBot',
+  branches: ["main"],
+  tagFormat: "${version}",
+  repositoryUrl: "https://github.com/cbackas/TVBot",
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
     [
-      '@semantic-release/github',
+      "@semantic-release/github",
       {
         failTitle: false,
         failComment: false,
         labels: false,
-        releasedLabels: false
-      }
+        releasedLabels: false,
+      },
     ],
-    [
-      '@codedependant/semantic-release-docker',
-      {
-        dockerTags: ['{{version}}', 'latest'],
-        dockerFile: 'Dockerfile',
-        dockerPlatform: ['linux/amd64', 'linux/arm64'],
-        dockerRegistry: 'ghcr.io',
-        dockerProject: 'cbackas',
-        dockerImage: 'tvbot'
-      }
-    ]
   ],
-  preset: 'eslint'
+  preset: "eslint",
 }
