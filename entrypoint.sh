4857d09cbc90219e073200821e6f967939ed8d1e
cbackas <git@zac.gg>
2025-04-25 16:14:04 -0500

    always use the DENO_KV_SQLITE_PATH env var to set the path to the kv sqlite file

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ if ! test -d /data; then
 	mkdir /data
 fi
 if ! test -f "$DENO_KV_SQLITE_PATH"; then
-	litestream restore --if-replica-exists /data/denokv.sqlite3
+	litestream restore --if-replica-exists -o "$DENO_KV_SQLITE_PATH"
 fi
 
 litestream replicate -exec 'tvbot'
